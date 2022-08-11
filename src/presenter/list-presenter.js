363f11016e51bb35913cce302cf5def0f6f99fc8
KseniaKrasilnikova <tksenia13@gmail.com>
2022-08-11 19:46:19 +0300

    рефактор презентера

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -2,31 +2,31 @@ import AddNewPointView from '../view/add-new-point-view.js';
 import EditPointView from '../view/edit-point-view.js';
 
 import ListView from '../view/list-view.js';
-import PointView from '../view/point-view.js';
 import ListItemView from '../view/list-item-view.js';
+
+import PointView from '../view/point-view.js';
 import {render} from '../render';
 
 export default class ListPresenter {
   pointsList = new ListView();
 
   init = (listContainer) => {  // где мы это все будем  делать
-    this.listContainer = listContainer;
-    const listItem =  new ListItemView();
-    const listAddPointItem =  new ListItemView();
 
-    render(this.pointsList, this.listContainer);
+    render(this.pointsList, listContainer);
 
-    render(listItem, this.pointsList.getElement());
-    render(new EditPointView(), listItem.getElement());
+    for (let i = 0; i < 5; i++) {
+      const listItem =  new ListItemView();
 
-    render(listAddPointItem, this.pointsList.getElement());
-    render(new AddNewPointView(), listAddPointItem.getElement());
-
-    for (let i = 0; i < 3; i++) {
-      this.listItem =  new ListItemView();
-
-      render(this.listItem, this.pointsList.getElement());
-      render(new PointView(), this.listItem.getElement());
+      render(listItem, this.pointsList.getElement());
+      let listItemContent = null;
+      if (i === 0) {
+        listItemContent = new EditPointView()
+      } else if (i === 1) {
+        listItemContent = new AddNewPointView()
+      } else {
+        listItemContent = new PointView()
+      }
+      render(listItemContent, listItem.getElement());
     }
   };
 }
