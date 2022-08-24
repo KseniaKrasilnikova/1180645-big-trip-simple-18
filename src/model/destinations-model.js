97a4ce39a10128b033682b148b8f394c9203092d
KseniaKrasilnikova <tksenia13@gmail.com>
2022-08-24 13:53:11 +0300

    удаляет destination-model и решает конфликт

--- src/model/destinations-model.js
+++
@@ -1,7 +0,0 @@
-import {generateDestination} from '../mock/mock-destination.js';
-
-export default class DestinationsModel {
-  destinations = Array.from({length: 3}, generateDestination);
-
-  getDestinations= () => this.destinations;
-}
