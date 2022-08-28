0fb144e8140e96f250d23d4effc209c7e1983341
KseniaKrasilnikova <tksenia13@gmail.com>
2022-08-28 13:06:06 +0300

    удаляет лишнее

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -1,6 +1,6 @@
 import {render} from '../render';
 import PointModel from '../model/point-model';
-// import AddNewPointView from '../view/add-new-point-view';
+import AddNewPointView from '../view/add-new-point-view';
 import EditPointView from '../view/edit-point-view';
 import ListView from '../view/list-view';
 import PointView from '../view/point-view';
@@ -13,12 +13,11 @@ export default class ListPresenter {
   #sortView = new SortView();
   #pointsList = new ListView();
   #listContainer = null;
-  #pointsModel = new PointModel;
+  #pointsModel = new PointModel();
   #points = [];
 
   init = (listContainer) => {
     this.#listContainer = listContainer;
-    this.#pointsModel = new PointModel(); // создаем новый объект из класса
     this.#points = [...this.#pointsModel.points]; // вызываем-создаем все поинты, к-ые мы нагенерировали
 
     if (this.#points.length === 0) {
@@ -30,12 +29,12 @@ export default class ListPresenter {
       for (let i = 0; i < this.#points.length; i++) {
         this.#renderPoint(this.#points[i]); // отрисовали все карточки
       }
-      // render(new AddNewPointView (
-      //   this.#pointsModel.addPoint,
-      //   this.#pointsModel.offers,
-      //   this.#pointsModel.getPointOffers(this.#pointsModel.addPoint),
-      //   this.#pointsModel.getPointDestination(this.#pointsModel.addPoint)
-      // ), this.#pointsList.element);
+      render(new AddNewPointView (
+        this.#pointsModel.addPoint,
+        this.#pointsModel.offers,
+        this.#pointsModel.getPointOffers(this.#pointsModel.addPoint),
+        this.#pointsModel.getPointDestination(this.#pointsModel.addPoint)
+      ), this.#pointsList.element);
     }
   };
 
