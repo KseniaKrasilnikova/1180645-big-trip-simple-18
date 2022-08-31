d7efc3f2e44194e1274555b9a3ccd8fa90948b8b
KseniaKrasilnikova <tksenia13@gmail.com>
2022-08-31 11:00:35 +0300

    выделяет в презентере рендеринг компонентов

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -1,4 +1,4 @@
-import {render} from '../framework/render';
+import {render, RenderPosition} from '../framework/render';
 import PointModel from '../model/point-model';
 import AddNewPointView from '../view/add-new-point-view';
 import EditPointView from '../view/edit-point-view';
@@ -15,29 +15,35 @@ export default class ListPresenter {
   #listContainer = null;
   #pointsModel = new PointModel();
   #points = [];
+  #noPointsComponent = new NoPointsView();
+  #siteTripEventsElement = null;
 
   init = (listContainer) => {
     this.#listContainer = listContainer;
+    this.#siteTripEventsElement = siteTripEventsElement;
     this.#points = [...this.#pointsModel.points]; // вызываем-создаем все поинты, к-ые мы нагенерировали
 
     if (this.#points.length === 0) {
-      render(new NoPointsView(), this.#listContainer);
+      this.#renderNoPoints();
     } else {
-      render(this.#sortView, siteTripEventsElement);
-      render(this.#pointsList, this.#listContainer);
-
+      this.#renderSortView();
+      this.#renderList();
       for (let i = 0; i < this.#points.length; i++) {
         this.#renderPoint(this.#points[i]); // отрисовали все карточки
       }
-      render(new AddNewPointView (
-        this.#pointsModel.addPoint,
-        this.#pointsModel.offers,
-        this.#pointsModel.getPointOffers(this.#pointsModel.addPoint),
-        this.#pointsModel.getPointDestination(this.#pointsModel.addPoint)
-      ), this.#pointsList.element);
+      this.#renderAddNewPoint(this.#pointsModel.addPoint);
     }
   };
 
+  #renderNoPoints = () => {
+    render(this.#noPointsComponent, this.#listContainer, RenderPosition.AFTERBEGIN);
+  };
+  #renderSortView = () => {
+    render(this.#sortView, this.#siteTripEventsElement, RenderPosition.AFTERBEGIN);
+  };
+  #renderList = () => {
+    render(this.#pointsList, this.#listContainer, RenderPosition.BEFOREEND);
+  };
   #renderPoint = (point) => {
     const pointComponent = new PointView( // создает экземпляр PointView
       point,
@@ -54,11 +60,9 @@ export default class ListPresenter {
     const replacePointToEditForm = () => {
       this.#pointsList.element.replaceChild(editPointComponent.element, pointComponent.element);
     };
-
     const replaceEditFormToPoint = () => {
       this.#pointsList.element.replaceChild(pointComponent.element, editPointComponent.element);
     };
-
     const onEscKeyDown = (evt) => {
       if (evt.key === 'Escape' || evt.key === 'Esc') {
         evt.preventDefault();
@@ -66,16 +70,13 @@ export default class ListPresenter {
         document.removeEventListener('keydown', onEscKeyDown);
       }
     };
-
     pointComponent.setClickHandler(() => {
       replacePointToEditForm();
       document.addEventListener('keydown', onEscKeyDown);
     });
-
     editPointComponent.setClickHandler(() => {
       replaceEditFormToPoint();
     });
-
     editPointComponent.setFormSubmitHandler((evt) => {
       evt.preventDefault();
       replaceEditFormToPoint();
@@ -83,5 +84,15 @@ export default class ListPresenter {
     });
 
     render(pointComponent, this.#pointsList.element); // отрисовывает в нужное место
+  };
+  #renderAddNewPoint = (point) => {
+    const addNewPointComponent = new AddNewPointView( // создает экземпляр AddNewPointView
+      point,
+      this.#pointsModel.offers,
+      this.#pointsModel.getPointOffers(point),
+      this.#pointsModel.getPointDestination(point)
+    );
+
+    render(addNewPointComponent, this.#pointsList.element); // отрисовывает в нужное место
   };
 }
